d5654f36173a0b4c9e7f8c940a495f01444e401a
MaestroTool <51473403+MaestroTool@users.noreply.github.com>
2019-06-26 11:50:42 +0700

    Bgsd

---
+++ TolBgSd.sh
@@ -0,0 +1,379 @@
+#usr/bin/bash
+clear
+# Jangan RECODE Gan, CPEK LO BUATNYA ðŸ™‚
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+#ini paket untuk menjalankan tools
+BGSDKW.sh
+echo
+clear
+echo
+echo "1%"
+sleep 0.1
+clear
+echo $me"2%"
+sleep 0.1
+clear
+echo "3%"
+sleep 0.1
+clear
+echo "4%"
+sleep 0.1
+clear
+echo "8%"
+sleep 0.2
+clear
+echo "9%"
+sleep 0.3
+clear
+echo "11%"
+sleep 0.4
+clear
+echo $i"15%"
+sleep 1
+clear
+echo $me"18%"
+sleep 0.2
+clear
+echo "22%"
+sleep 3
+clear
+echo "26%"
+sleep 0.6
+clear
+echo "27%"
+sleep 2
+clear
+echo "30%"
+sleep 3
+clear
+echo $pu"34%"
+sleep 5
+clear
+echo "35%"
+sleep 0.3
+clear
+echo "100%"
+sleep 3
+clear
+cowsay Maestro
+toilet -f standard "MRB4NGS4D" -F gay
+sleep 2
+
+echo
+echo $pur "_______________________________________________"
+echo $pur "|||||||||||||||||||||||||||||||||||||||||||||||||"
+echo $pir " -----------------------------------------------"
+echo $i  "*" $cy"Author     : "$me" MAESTRO"
+echo $i  "*" $cy"Type       : "$me" Tools Installer"
+echo $i  "*" $cy"Facebook   : "$me" www.facebook.com/MAESTRO10001"
+echo $pur "_______________________________________________"
+echo $pur "|||||||||||||||||||||||||||||||||||||||||||||||||"
+echo $pur "         -----------------------------------------------"
+
+echo
+sleep 5
+echo $me "[+]=============[Tool MR-B4NGS4D]=============[+]"
+echo $cy "[1]"$bi" Brute Force Facebook"                #usr/bin/bash
+clear
+
+# Jangan RECODE Gan, CPEK LO BUATNYA
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+echo
+echo
+#ini paket untuk menjalankan tools
+DinoSec.sh
+echo
+clear
+echo
+echo
+echo $ku "10%  [Working]"
+sleep 0.1
+clear
+echo "50%  [Working]"
+sleep 0.1
+clear
+echo "100% [Working]"
+sleep 2
+clear
+echo "100%  [Berhasil]"
+sleep 3
+clear
+
+echo $me"â€¢"
+sleep 1
+echo "â€¢â€¢"
+sleep 1
+echo "â€¢â€¢â€¢â€¢â€¢"
+sleep 2
+echo "â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 0.1
+echo "â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢"
+sleep 2
+clear
+echo "1%"
+sleep 0.1
+clear
+echo "2%"
+sleep 0.1
+clear
+echo "3%"
+sleep 0.1
+clear
+echo "4%"
+sleep 0.1
+clear
+echo "5%"
+sleep 0.1
+clear
+echo "6%"
+sleep 0.1
+clear
+echo "7%"
+sleep 0.1
+clear
+echo "8%"
+sleep 0.1
+clear
+echo "9%"
+sleep 0.1
+clear
+figlet 1%
+sleep 0.1
+clear
+figlet 2%
+sleep 0.1
+clear
+figlet 100%
+sleep 2
+clear
+toilet -f standard "MRB4NGS4D" -F gay
+sleep 9
+clear
+cowsay Apa Lo Anjing
+echo
+echo $pur "_____________________"
+sleep 0.1
+echo $i  "*" $cy"Author     : "$me" MAESTRO"
+sleep 0.1
+echo $i  "*" $cy"Type       : "$me" Tools Installer"
+sleep 0.1
+echo $i  "*" $cy"Facebook   : "$me" www.facebook.com/MAESTRO10001"
+
+sleep 0.1
+echo $pur ".                               ________________"
+sleep 0.1
+
+echo $bi  "|________________________________________________|"
+sleep 0.1
+echo $bi  "|"$pu"Sebelum menggunakan Tools Ini silahkan install b"$bi"|"
+sleep 0.1
+echo $bi  "|"$pu"ahan Dulu agar semua Toolsnya Work tidak ERORR!!"$bi"|"
+sleep 0.1
+echo $bi  "\_________________"$me"[WARNING!]"$bi"_____________________/"
+echo
+
+sleep 5
+echo $pu"_________________________________________________"
+echo $me "[\/]"$cy"||||||||||||||||||||||||||||||||||||||||||"$me"[\/]"
+echo $me "[\/]"$cy"|TOOL FOR FACEBOOK|"$me"[\/]"
+echo $me "[\/]"$cy"||||||||||||||||||||||||||||||||||||||||||"$me"[\/]"
+echo
+sleep 2
+echo $me"[â€¢|â€¢]"$bi"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$me"[â€¢|â€¢]"
+sleep 0.1
+echo $pu"________________________________________________________"
+sleep 0.1
+echo $pu"|" $cy"1."$pu"|"$pur"DarkFB (v1.6)"
+echo $pu"|__________________________________________|"$me"M"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"2."$pu"|"$pur"AutoReport"
+sleep 0.1
+echo $pu"|__________________________________________|"$me"A"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"3."$pu"|"$pur"Multi Brute Force"
+sleep 0.1
+echo $pu"|__________________________________________|"$me"E"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"4."$pu"|"$pur"Brute Force Target"
+sleep 0.1
+echo $pu"|__________________________________________|"$me"S"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"5."$pu"|"$pur"Profil Guard"
+sleep 0.1
+echo $pu"|__________________________________________|"$me"T"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"6."$pu"|"$pur"Auto Komen"
+sleep 0.1
+echo $pu"|__________________________________________|"$me"R"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"7."$pu"|"$pur"Auto Reaction"
+sleep 0.1
+echo $pu"|__________________________________________"$pu"|"$me"O"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"8."$pu"|"$pur"Hapus Pertemanan (Otomatis)"
+sleep 0.1
+echo $pu"|__________________________________________"$pu"|"$me"*"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"9."$pu"|"$me"Install bahan"
+sleep 0.1
+echo $pu"|__________________________________________"$pu"|"$me"*"$pu"|"
+sleep 0.1
+echo $pu"|" $cy"00."$pu"|"$pur"Exit"
+sleep 0.1
+echo $pu"|_________________________________________________________"
+sleep 0.1
+read -p"root@MR-B4NGS4D: ~# " pil;
+
+if [ $pil = 1 ]
+then
+clear
+cowsay Apa LO Anjing
+sleep 4
+clear
+git clone https://github.com/mrb4n6547/Bangsad.git
+cd Bangsad $y
+python2 BgSdKw.py $y
+fi
+
+if [ $pil = 2 ]
+then
+clear
+cowsay Apa Lo Lonte
+sleep 4
+clear
+git clone https://github.com/IlayTamvan/Report.git
+cd Report $y
+unzip Report.zip $y
+python2 Report.py $y
+fi
+
+if [ $pil = 4 ]
+then
+clear
+cowsay Apa Lo Kontol
+sleep 4
+clear
+git clone https://github.com/MaestroTool/HackFB.git
+cd HackFB $y
+python2 hack.py $y
+fi
+
+if [ $pil = 3 ]
+then
+clear
+cowsay Apa Lo BABI
+sleep 4
+clear
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute $y
+python2 MBF.py $y
+if
+
+if [ $pil = 5 ]
+then
+clear
+cowsay Apa Lo Monyet
+sleep 4
+clear
+git clone https://github.com/MaestroTool/MR-B4NGS4D.git
+cd MR-B4NGS4D $y
+php GuardFBAsw.php $y
+fi
+
+if [ $pil = 6 ]
+then
+clear
+cowsay Apa Lo Lonte
+sleep 4
+clear
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkemana $y
+python2 botkomena.py $y
+fi
+
+if [ $pil = 7 ]
+then
+clear
+cowsay Apa Lo BABI 
+sleep 4
+clear
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF $y
+pip2 install -r requirements.txt $y
+python2 osif.py $y
+fi
+
+if [ $pil = 8 ]
+then
+clear
+cowsay Apa Lo Kontol 
+sleep 4
+clear
+git clone https://github.com/dandyraka/FacebookUnfriend.git
+cd FacebookUnfriend $y
+php unfriend.php $y
+fi
+
+if [ $pil = 9 ]
+then
+clear
+cowsay Apa Lo Lonte 
+sleep 4
+pkg install python python2 python3 $y
+pkg install php $y
+pkg install curl $y
+pkg install php $y
+pkg install lolcat $y
+pkg install cowsay $y
+pkg install toilet $y
+pkg install requests $y
+pip2 install requests $y
+pkg install mechanize $y
+pip2 install mechanize $y
+sleep 5
+echo "Semua sudah TerInstall..."
+sleep 2
+echo "Silahlan Jalan kan ulang Tool ini seperti tadi :)"
+sleep 4
+echo "E X I T"
+exit
+fi
+
+if [ $pil = 00 ]
+then
+clear
+echo " Keluar"
+exit
+fi
+
